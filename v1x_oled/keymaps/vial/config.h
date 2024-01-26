035e7563c27c3d8082c156816f40150e38eb4955
sourav-txt <49057757+sourav-txt@users.noreply.github.com>
2024-01-26 09:59:28 +0530

    Add files via upload
    Initial Commit

---
+++ v1x_oled/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x14, 0x68, 0x6D, 0x85, 0x1B, 0x3A, 0x43, 0x2B}
+
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_TAPPING
+#define NO_MUSIC_MODE
